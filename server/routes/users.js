6fa3d81d16c27e1e9ffa59d2d875381b7302c969
ranoa <ranoaritsiky@gmail.com>
2023-01-30 23:15:04 +0300

    init prisma and imp create and read all

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,9 +1,50 @@
 var express = require('express');
 var router = express.Router();
+const prisma = require('../db/prisma')
+
+
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+router.get('/', async (req, res) => {
+  const users = await prisma.user.findMany()
+  res.send(users)
+})
+
+/* POST users listing. */
+router.post('/create_user', async(req, res) => {
+  const { name, email, password } = req.body
+
+  // check if email does not exist yet
+  const haveEmail = await prisma.user.findMany(
+    {
+      where: {
+        email : email
+      }
+    }
+  )
+  if (haveEmail.length > 0) {
+    res.status(400).json({
+      status: "Failed",
+      message:"Email already exist"
+    })
+  }
+  else {
+    await prisma.user.create({
+      data: {
+        name: name,
+        email: email,
+        password: password
+      }
+    })
+      res.status(200).json({
+        status: "Success",
+        message :"User was created successfuly"
+        
+      })
+    
+  }
+
+})
+
 
 module.exports = router;
