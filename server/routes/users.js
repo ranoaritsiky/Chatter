ce47250d60d69dd551f6d36ad7a3b01f39da70df
ranoa <ranoaritsiky@gmail.com>
2023-02-01 23:05:52 +0300

    finalyse crud

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,8 +1,16 @@
+const {PrismaClient} = require('@prisma/client')
+const prisma = new PrismaClient()
 var express = require('express');
 var router = express.Router();
-const prisma = require('../db/prisma')
 
-
+const getUserByEmail = async (email) => {
+  console.log(email)
+  return await prisma.user.findUnique({
+    where: {
+      email: email,
+    },
+  })
+}
 
 /* GET users listing. */
 router.get('/', async (req, res) => {
@@ -45,6 +53,53 @@ router.post('/create_user', async(req, res) => {
   }
 
 })
+
+router.put('/update_user', async (req, res) => {
+  const { email, name } = req.body
+  
+  const user = await getUserByEmail(email);
+  if (!user) {
+    res.status(404).json({
+      error: 'User not found'
+    });
+  } else {
+    await prisma.user.update({
+      where: {
+        email: email
+      },
+      data:{
+        name :name
+      }
+    })
+    res.status(200).json({
+      message: 'User was updated successfuly'
+    })
+  }
+  
+})
+
+router.delete('/delete_user', async (req, res) => {
+  const {email} = req.body
+  const user = await getUserByEmail(email);
+
+  if (!user) {
+    res.status(404).json({
+      error: 'User not found'
+    });
+  } else {
+    await prisma.user.delete({
+      where: {
+        email: email
+      }
+    })
+    res.status(200).json({
+      message: 'User was deleted successfuly'
+    })
+  }
+
+  })
+
+
 
 
 module.exports = router;
